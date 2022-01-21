f432f72dddd1197e5eea402295380e0d0438ceb5
Simon Bustamante <simon.bustamante@gmail.com>
2022-01-21 18:04:59 -0500

    security user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Activity;
+use App\Entity\B2CProductRequest;
 use App\Entity\Farm;
+use App\Entity\FarmerBalance;
+use App\Entity\FarmerLoans;
 use App\Entity\FarmerRegister;
 use App\Entity\FarmInventory;
 use App\Entity\Group;
 use App\Entity\InventoryUpdate;
 use App\Entity\Location;
+use App\Entity\MayaniLoanProducts;
+use App\Entity\MayaniProductInventory;
+use App\Entity\MayaniRequestInventory;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -47,7 +53,13 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Activity', 'fas fa-list', Activity::class);
         yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
         yield MenuItem::linkToCrud('Farm Inventory', 'fas fa-list', FarmInventory::class);
-        yield MenuItem::linkToCrud('Inventory Update', 'fas fa-list', InventoryUpdate::class);
+        yield MenuItem::linkToCrud('Request Inventory Update', 'fas fa-list', InventoryUpdate::class);
+        yield MenuItem::linkToCrud('Mayani Request Inventory', 'fas fa-list', MayaniRequestInventory::class);
+        yield MenuItem::linkToCrud('Mayani Product Inventory', 'fas fa-list', MayaniProductInventory::class);
+        yield MenuItem::linkToCrud('B2C Product Request', 'fas fa-list', B2CProductRequest::class);
+        yield MenuItem::linkToCrud('Farmers Balance', 'fas fa-list', FarmerBalance::class);
+        yield MenuItem::linkToCrud('Farmer Loans', 'fas fa-list', FarmerLoans::class);
+        yield MenuItem::linkToCrud('Mayani\'s Loan Products ', 'fas fa-list', MayaniLoanProducts::class);
         yield MenuItem::linkToCrud('Farmers Register', 'fas fa-list', FarmerRegister::class);
         yield MenuItem::linkToCrud('Farmers Register', 'fas fa-list', FarmerRegister::class);
         yield MenuItem::linkToCrud('Farmers Register', 'fas fa-list', FarmerRegister::class);
